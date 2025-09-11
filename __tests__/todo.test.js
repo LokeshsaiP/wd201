e48366b0a9d79c3acffddbe2d999a22d9d931339
Lokesh Sai <lokeshsai@Lokeshs-MacBook-Air.local>
2025-09-11 11:37:36 +0530

    add pre-commit hook to run tests

---
+++ __tests__/todo.test.js
@@ -0,0 +1,27 @@
+const todoList = require("../todo-cli/todo");
+
+const { all, markAsComplete, add } = todoList();
+
+describe("First test suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toISOString().split("T")[0],
+    });
+  });
+  test("should add a new todo", () => {
+    const len = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toISOString().split("T")[0],
+    });
+    expect(all.length).toBe(len + 1);
+  });
+  test("should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});

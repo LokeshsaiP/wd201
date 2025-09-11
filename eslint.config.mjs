58ce4eb034aa94787b8375bf3c6e0281187fc9c7
Lokesh Sai <lokeshsai@Lokeshs-MacBook-Air.local>
2025-09-11 13:39:07 +0530

    Add lint staged

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  tseslint.configs.recommended,
+  {
+    env: {
+      commonjs: true,
+      es2021: true,
+      node: true,
+      jest: true,
+    },
+    extends: "eslint:recommended",
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: "latest",
+    },
+    rules: {},
+  },
+]);

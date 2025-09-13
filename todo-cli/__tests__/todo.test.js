62c61079cb588a6d35eac39d3495a01972a9fae1
Lokesh Sai <lokeshsai@Lokeshs-MacBook-Air.local>
2025-09-13 12:00:05 +0530

    add relevant tests

--- todo-cli/__tests__/todo.test.js
+++ todo-cli/__tests__/todo.test.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("First test suite", () => {
-  beforeAll(() => {
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toISOString().split("T")[0],
-    });
-  });
+  // beforeAll(() => {
+  //   add({
+  //     title: "Test todo",
+  //     completed: false,
+  //     dueDate: new Date().toISOString().split("T")[0],
+  //   });
+  // });
   test("should add a new todo", () => {
     const len = all.length;
     add({
@@ -24,5 +24,39 @@ describe("First test suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("should retrieve overdue items", () => {
+    let yesterday = new Date(new Date().setDate(new Date().getDate() - 1))
+      .toISOString()
+      .split("T")[0];
+    add({
+      title: "Test todo overdue",
+      completed: false,
+      dueDate: yesterday,
+    });
+    let overdues = overdue();
+    expect(overdues.length).toBe(1);
+  });
+  test("should retrieve due today items", () => {
+    let today = new Date().toISOString().split("T")[0];
+    add({
+      title: "Test todo due today",
+      completed: false,
+      dueDate: today,
+    });
+    let itemsDueToday = dueToday();
+    expect(itemsDueToday.length).toBe(2);
+  });
+  test("should retrieve due later items", () => {
+    let tomorrow = new Date(new Date().setDate(new Date().getDate() + 1))
+      .toISOString()
+      .split("T")[0];
+    add({
+      title: "Test todo due later",
+      completed: false,
+      dueDate: tomorrow,
+    });
+    let itemsDueLater = dueLater();
+    expect(itemsDueLater.length).toBe(1);
   });
 });
